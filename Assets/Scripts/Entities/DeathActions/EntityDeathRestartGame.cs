520ec700ac22f8f7f6c46907f7b02640e8c45b8a
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-12-05 13:35:36 +0100

    restart game now has delay

--- Assets/Scripts/Entities/DeathActions/EntityDeathRestartGame.cs
+++ Assets/Scripts/Entities/DeathActions/EntityDeathRestartGame.cs
@@ -1,11 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Entities
 {
     public class EntityDeathRestartGame : EntityDeathAction
     {
+        public float delay = 1;
         protected override void OnDeath()
         {
+            StartCoroutine(DelayThenReset());
+        }
+
+        IEnumerator DelayThenReset()
+        {
+            yield return new WaitForSeconds(delay);
             StartCoroutine(GameManager.instance.ResetGame());
         }
     }
