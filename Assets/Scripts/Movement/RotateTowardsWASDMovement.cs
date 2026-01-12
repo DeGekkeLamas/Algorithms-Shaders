af7c69fb4575777037821ac4732cb6e6a733965d
DeGekkeLamas <karsvdtoorn@gmail.com>
2026-01-12 10:39:29 +0100

    player now has a model and it rotates towards movement

---
+++ Assets/Scripts/Movement/RotateTowardsWASDMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RotateTowardsWASDMovement : MonoBehaviour
+{
+    void Update()
+    {
+        float camAngle = Camera.main.transform.eulerAngles.y;
+        Vector3 movement = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement = VectorMath.RotateVectorXZ(movement, -camAngle);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+}
