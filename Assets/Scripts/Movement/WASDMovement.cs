c63dcab18f2e2b2d45554e3b3e9506f1c7b20d48
DeGekkeLamas <karsvdtoorn@gmail.com>
2026-01-12 10:56:09 +0100

    minor movement fix

--- Assets/Scripts/Movement/WASDMovement.cs
+++ Assets/Scripts/Movement/WASDMovement.cs
@@ -13,7 +13,7 @@ namespace MovementStuff
             Vector3 movement = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             movement = VectorMath.RotateVectorXZ(movement, -camAngle);
 
-            if (!Physics.Raycast(this.transform.position, movement, .5f))
+            if (!Physics.Raycast(this.transform.position + this.transform.localScale.y * .5f * Vector3.up, movement, .5f))
             {
                 transform.position += moveSpeed * baseSpeed * Time.deltaTime * movement;
             }
